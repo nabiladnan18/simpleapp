68f8e85e92dbcf5b98d569065a667a97ee9e577e
Nabil Adnan <nadnan@TB-L196.corp.ad.zalando.net>
2023-07-04 23:47:19 +0200

    simple hello world app

--- main.py
+++ main.py
@@ -1,25 +1,14 @@
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, session, redirect, url_for
 from dotenv import load_dotenv
 import os
 
 app = Flask(__name__, template_folder="./templates", static_folder="./static")
-
 load_dotenv(".env")
 
-
-@app.route("/", methods=["GET", "POST"])
-def home():
-    counter = session.get("counter", 0)
-    environment = os.getenv("FLASK_ENVIRONMENT")
-    if request.method == "POST":
-        counter += 1
-        session.update(
-            {
-                "counter": counter
-            }
-        )
-    
-    return render_template('base.html', counter=counter, environment=environment)
+@app.route('/')
+@app.route('/<name>')
+def home(name=None):
+        return render_template('base.html', name=name)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)

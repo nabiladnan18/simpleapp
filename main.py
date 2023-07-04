594fe4caf44dd5c974a3dd85434090186602370c
Nabil Adnan <nadnan@TB-L196.corp.ad.zalando.net>
2023-07-04 22:04:45 +0200

    Added counter feature

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__, template_folder="./templates", static_folder="./static")
 
-@app.route("/")
-def hello_world():
-    return render_template("base.html")
+load_dotenv(".env")
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    counter = session.get("counter", 0)
+    environment = os.getenv("FLASK_ENVIRONMENT")
+    if request.method == "POST":
+        counter += 1
+        session.update(
+            {
+                "counter": counter
+            }
+        )
+    
+    return render_template('base.html', counter=counter, environment=environment)
 
 if __name__ == '__main__':
     app.run()

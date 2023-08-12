f16ef0aa0553613f75a75338caaface31ccf87d9
Nabil Adnan <nadnan@TB-L196.corp.ad.zalando.net>
2023-08-12 21:45:31 +0200

    testing jenkins

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__, template_folder="./templates", static_folder="./static")
 
+
 @app.route('/', methods=['GET', 'POST'])
 def home(name=None):
     if request.method == 'POST':
@@ -9,13 +10,20 @@ def home(name=None):
         return redirect(url_for('user', name=name))
     return render_template('base.html')
 
+
 @app.route('/custom', methods=['GET', 'POST'])
 def custom_name():
     return render_template("name.html")
 
+
 @app.route('/<name>', methods=['GET', 'POST'])
 def user(name):
     return render_template('base.html', name=name)
+
+
+@app.route('/whoa', methods=['GET','POST'])
+def testing_pipeline():
+    pass
 
 if __name__ == '__main__':
     app.run()
